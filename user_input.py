ec1f8114c0c9eebc9786cbd22c29b1360da68265
aluochkure@gmail.com <aluochkure@gmail.com>
2024-11-05 10:24:50 +0300

    My first python assignment

---
+++ user_input.py
@@ -0,0 +1,9 @@
+# Asking for user input and storing it in variables
+#  Ask the user for their name
+name = input("What is your name? ")
+# Ask the user for their age
+age = input("What is your age? ")
+# Ask the user for their location
+location = input("Where do you live? ")
+# Displaying a personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
